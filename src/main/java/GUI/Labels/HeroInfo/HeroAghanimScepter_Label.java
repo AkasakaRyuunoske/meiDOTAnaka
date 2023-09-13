365c7992735b2de05d48feb6af78ea4444713e4c
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-13 16:01:37 +0200

    new component added

---
+++ src/main/java/GUI/Labels/HeroInfo/HeroAghanimScepter_Label.java
@@ -0,0 +1,9 @@
+package GUI.Labels.HeroInfo;
+
+import javax.swing.*;
+
+public class HeroAghanimScepter_Label extends JLabel {
+    public HeroAghanimScepter_Label(){
+        setText("None");
+    }
+}
