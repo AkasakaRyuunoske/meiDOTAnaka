0b3982d5f33a6f089805560c40140081a9341f57
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-13 16:02:42 +0200

    new component added

---
+++ src/main/java/GUI/Labels/PlayerInfo/PlayerInfoTitle_Label.java
@@ -0,0 +1,9 @@
+package GUI.Labels.PlayerInfo;
+
+import javax.swing.*;
+
+public class PlayerInfoTitle_Label extends JLabel {
+    public PlayerInfoTitle_Label(){
+        setText("Player Info:");
+    }
+}
