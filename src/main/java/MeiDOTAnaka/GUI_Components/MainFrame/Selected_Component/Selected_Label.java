1c70c901397a57741dbb66699ba15ae4a5b13a60
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:10:59 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Selected_Component/Selected_Label.java
@@ -0,0 +1,9 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Selected_Component;
+
+import javax.swing.*;
+
+public class Selected_Label extends JLabel {
+    public Selected_Label(){
+        setText("<Selected State>");
+    }
+}
