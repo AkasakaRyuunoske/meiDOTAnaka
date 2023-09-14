73b79e5889c360cab0ca25d5237b1990696d6720
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:25 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Buttons_Component/Buttons_Panel.java
@@ -0,0 +1,28 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Buttons_Panel extends JPanel {
+    Config_Button config_button;
+    CurrentGame_Button currentGame_button;
+    Graphs_Button graphs_button;
+    Players_Button players_button;
+    PostGame_Button postGame_button;
+
+    public Buttons_Panel(){
+        setLayout(new GridLayout(10, 1));
+
+        config_button = new Config_Button();
+        currentGame_button = new CurrentGame_Button();
+        graphs_button = new Graphs_Button();
+        players_button = new Players_Button();
+        postGame_button = new PostGame_Button();
+
+        add(config_button);
+        add(currentGame_button);
+        add(graphs_button);
+        add(players_button);
+        add(postGame_button);
+    }
+}
