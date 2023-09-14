e239366585d9d3976df26b0943b65ad8a1344721
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:20 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Buttons_Component/CurrentGame_Button.java
@@ -0,0 +1,10 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component;
+
+import javax.swing.*;
+
+public class CurrentGame_Button  extends JButton {
+    public CurrentGame_Button(){
+        setFocusable(false);
+        setText("Current Game");
+    }
+}
