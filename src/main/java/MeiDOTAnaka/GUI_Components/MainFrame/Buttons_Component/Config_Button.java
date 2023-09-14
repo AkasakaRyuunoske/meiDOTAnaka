6b41b254cec85535c6f82ab905003f5ec0ead742
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:23 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Buttons_Component/Config_Button.java
@@ -0,0 +1,10 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component;
+
+import javax.swing.*;
+
+public class Config_Button  extends JButton {
+    public Config_Button(){
+        setFocusable(false);
+        setText("Config");
+    }
+}
