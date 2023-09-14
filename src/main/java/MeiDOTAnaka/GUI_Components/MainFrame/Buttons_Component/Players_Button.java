80f52bcb8aeb6edfdcf1a1196c592ffac5d0b606
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:05 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Buttons_Component/Players_Button.java
@@ -0,0 +1,10 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component;
+
+import javax.swing.*;
+
+public class Players_Button  extends JButton {
+    public Players_Button(){
+        setFocusable(false);
+        setText("Players");
+    }
+}
