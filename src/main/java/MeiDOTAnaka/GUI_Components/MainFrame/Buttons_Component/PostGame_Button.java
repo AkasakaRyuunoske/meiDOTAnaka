6bec56e5a921746903bc91de2df1e075351b3fb5
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:02 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Buttons_Component/PostGame_Button.java
@@ -0,0 +1,11 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component;
+
+import javax.swing.*;
+
+public class PostGame_Button  extends JButton {
+    public PostGame_Button(){
+        setFocusable(false);
+
+        setText("Post Game");
+    }
+}
