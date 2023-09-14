3ad9a583825878d24a8c9c23dc2406b505178e63
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:17 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Buttons_Component/Graphs_Button.java
@@ -0,0 +1,10 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component;
+
+import javax.swing.*;
+
+public class Graphs_Button  extends JButton {
+    public Graphs_Button(){
+        setFocusable(false);
+        setText("Graphs");
+    }
+}
