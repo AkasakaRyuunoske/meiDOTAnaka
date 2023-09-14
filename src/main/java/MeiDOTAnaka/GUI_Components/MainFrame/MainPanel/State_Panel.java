5e4634ccbaef45461376a64693c1e1657c662ed1
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:10:54 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/MainPanel/State_Panel.java
@@ -0,0 +1,11 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.MainPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class State_Panel extends JPanel {
+    public State_Panel(){
+        setLayout(new GridBagLayout());
+        setBackground(Color.BLACK);
+    }
+}
