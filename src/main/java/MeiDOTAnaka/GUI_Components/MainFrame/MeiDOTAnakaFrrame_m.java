4769627c6cbef9e093f1a6b60e59a3ff01257811
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:08 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/MeiDOTAnakaFrrame_m.java
@@ -0,0 +1,47 @@
+package MeiDOTAnaka.GUI_Components.MainFrame;
+
+import MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component.Buttons_Panel;
+import MeiDOTAnaka.GUI_Components.MainFrame.MainPanel.State_Panel;
+import MeiDOTAnaka.GUI_Components.MainFrame.Selected_Component.Selected_Panel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MeiDOTAnakaFrrame_m extends JFrame {
+    Selected_Panel selected_panel;
+    State_Panel state_panel;
+    Buttons_Panel buttons_panel;
+    public MeiDOTAnakaFrrame_m(){
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        setSize(1080, 720);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        setTitle("MeiDOTAnata - Your cringe assistant wirten by cringe ukrainian refugee");
+
+        selected_panel = new Selected_Panel();
+        state_panel = new State_Panel();
+        buttons_panel = new Buttons_Panel();
+
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 0.1; // Panel takes 10% of the screen width
+        gridBagConstraints.weighty = 1.0; // Both panels take full vertical space
+
+        add(buttons_panel, gridBagConstraints);
+
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = GridBagConstraints.NORTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 0.1;
+
+        state_panel.add(selected_panel, gridBagConstraints);
+
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0; // Panel takes 70% of the screen width
+        gridBagConstraints.weighty = 1.0;
+
+        add(state_panel, gridBagConstraints);
+
+        setVisible(true);
+    }
+}
