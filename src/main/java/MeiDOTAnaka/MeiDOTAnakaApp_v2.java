e1c542dde4eee9eb05d366307335c3ff00a21378
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-14 16:11:10 +0200

    New project structure

---
+++ src/main/java/MeiDOTAnaka/MeiDOTAnakaApp_v2.java
@@ -0,0 +1,9 @@
+package MeiDOTAnaka;
+
+import MeiDOTAnaka.GUI_Components.MainFrame.MeiDOTAnakaFrrame_m;
+
+public class MeiDOTAnakaApp_v2 {
+    public static void main(String[] args) {
+        new MeiDOTAnakaFrrame_m();
+    }
+}
