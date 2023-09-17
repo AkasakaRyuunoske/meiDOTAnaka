609975df050a8675155176982d233dcdd87ba41d
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-17 17:01:47 +0200

    resource renamed

--- src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Labels/PlayerInfo_Labels/PlayerInfo_Label.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Labels/PlayerInfo_Labels/PlayerInfo_Label.java
@@ -2,8 +2,8 @@ package MeiDOTAnaka.GUI_Components.CurrentGame.Labels.PlayerInfo_Labels;
 
 import javax.swing.*;
 
-public class PlayerInfoTitle_Label extends JLabel {
-    public PlayerInfoTitle_Label(){
+public class PlayerInfo_Label extends JLabel {
+    public PlayerInfo_Label(){
         setText("Player Info:");
     }
 }
