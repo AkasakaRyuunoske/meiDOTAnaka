22033aa985e8ec2510e3915ce4c9816a90d6da5a
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-17 22:37:24 +0200

    test image added

--- src/main/java/MeiDOTAnaka/GUI_Components/PostGame/Panels/HeroImportantStatsPanel/Hero_Panel.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/PostGame/Panels/HeroImportantStatsPanel/Hero_Panel.java
@@ -4,9 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Hero_Panel extends JPanel {
+    JLabel hero_image;
+    Image image;
+    ImageIcon imageIcon;
     public Hero_Panel(){
         setBackground(Color.BLUE);
-        JLabel hero_image = new JLabel();
-//        hero_image.setIcon(new ImageIcon(Hero_Panel.class.getResource("Resources\\Images\\shivallan_imba_hero_omg_volvo_fix_please.png")));
+
+        hero_image = new JLabel();
+        imageIcon = new ImageIcon("src\\main\\java\\MeiDOTAnaka\\Resources\\Images\\shivallan_imba_hero_omg_volvo_fix_please.png");
+
+        hero_image.setIcon(imageIcon);
+
+        add(hero_image);
     }
 }
