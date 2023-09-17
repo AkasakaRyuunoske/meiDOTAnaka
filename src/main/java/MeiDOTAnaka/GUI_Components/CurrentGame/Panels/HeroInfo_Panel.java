26ef576b209e06e53c7465ca74538ca86987480b
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-17 17:03:40 +0200

    Post game components added together

--- src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Panels/HeroInfo_Panel.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Panels/HeroInfo_Panel.java
@@ -1,10 +1,36 @@
 package MeiDOTAnaka.GUI_Components.CurrentGame.Panels;
 
+import MeiDOTAnaka.GUI_Components.CurrentGame.Labels.HeroInfo_Labels.*;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class HeroInfo_Panel extends JPanel {
+    HeroInfo_Label heroInfo_label;
+    HeroAghanimScepter_Label heroAghanimScepter_label;
+    HeroAghanimShard_Label heroAghanimShard_label;
+
+    HeroHP_ProgressBar heroHP_progressBar;
+    HeroMP_ProgressBar heroMP_progressBar;
+    HeroLvl_Label heroLvl_label;
+    HeroName_Label heroName_label;
+
     public HeroInfo_Panel(){
-        setBackground(Color.GREEN);
+        heroAghanimScepter_label = new HeroAghanimScepter_Label();
+        heroAghanimShard_label = new HeroAghanimShard_Label();
+        heroHP_progressBar = new HeroHP_ProgressBar();
+        heroMP_progressBar = new HeroMP_ProgressBar();
+        heroLvl_label = new HeroLvl_Label();
+        heroName_label = new HeroName_Label();
+        heroInfo_label = new HeroInfo_Label();
+
+//        setBackground(Color.GREEN);
+        add(heroInfo_label);
+        add(heroAghanimScepter_label);
+        add(heroAghanimShard_label);
+        add(heroHP_progressBar);
+        add(heroMP_progressBar);
+        add(heroLvl_label);
+        add(heroName_label);
     }
 }
--- src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Panels/PlayerInfo_Panel.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Panels/PlayerInfo_Panel.java
@@ -1,10 +1,31 @@
 package MeiDOTAnaka.GUI_Components.CurrentGame.Panels;
 
+import MeiDOTAnaka.GUI_Components.CurrentGame.Labels.PlayerInfo_Labels.*;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class PlayerInfo_Panel  extends JPanel {
+    APM_Label apm_label;
+    GPM_Label gpm_label;
+    XPM_Label xpm_label;
+    Networth_Label networth_label;
+    PlayerInfo_Label playerInfo_label;
+
     public PlayerInfo_Panel(){
-        setBackground(Color.RED);
+        setLayout(new GridLayout(2,2));
+
+        apm_label = new APM_Label();
+        gpm_label = new GPM_Label();
+        xpm_label = new XPM_Label();
+        networth_label = new Networth_Label();
+        playerInfo_label = new PlayerInfo_Label();
+
+        add(apm_label);
+        add(gpm_label);
+        add(xpm_label);
+        add(networth_label);
+        add(playerInfo_label);
+
     }
 }
