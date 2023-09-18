46d8f8710a1554720e080bbefcf62fc47a2d4575
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-18 10:42:16 +0200

    main_v2 cleared

--- src/main/java/MeiDOTAnaka/MeiDOTAnakaApp_v2.java
+++ src/main/java/MeiDOTAnaka/MeiDOTAnakaApp_v2.java
@@ -1,57 +1,24 @@
 package MeiDOTAnaka;
 
-import java.io.File;
+import MeiDOTAnaka.Services.Dota2RestAPIs.Dota2MatchAPI_570.GetMatchDetails;
+import MeiDOTAnaka.Services.Dota2RestAPIs.Dota2MatchAPI_570.MatchDetailsParser;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
 
 public class MeiDOTAnakaApp_v2 {
-//    public static void main(String[] args) {
-//        GetMatchHistory getMatchHistory = new GetMatchHistory();
-//        try {
-//            getMatchHistory.getFullMatchHistoryById();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-////        new MeiDOTAnakaFrrame_m();
-//
-//
-//    }
-    static int spaces = 0;
-    static int directory_spaces = 0;
-    static String space = " ";
-
     public static void main(String[] args) {
-        File currentDir = new File("./src");
-        displayDirectory(currentDir);
-    }
-
-    public static void displayDirectory(File dir) {
-        File[] files = dir.listFiles();
-
-        // For-each loop for iteration
-        for (File file : files) {
-
-            // Checking of file inside directory
-            if (file.isDirectory()) {
-
-                // Display directories inside directory
-
-                if (directory_spaces > 0)  System.out.println(space.repeat(directory_spaces) + "|" + space.repeat(spaces - directory_spaces) + "|---+ " + file.getName());
-                else System.out.println(space.repeat(spaces) + "|---+ " + file.getName());
-
-                if (file.getName().equals("GUI_Components")) directory_spaces = spaces;
-
-                spaces += 4;
-                displayDirectory(file);
-
-            } else {
-//                if (spaces >= 4) System.out.println(space.repeat(spaces - 4) + "|" + space.repeat(spaces - 8) + "|---# " + file.getName());
-                if (directory_spaces > 0 && (spaces - directory_spaces) >= 4)  System.out.println(space.repeat(directory_spaces) + "|" + space.repeat(spaces - directory_spaces) + "|---+ " + file.getName());
-                else System.out.println(space.repeat(spaces) + "|---#> " + file.getName());
-//                System.out.println(space.repeat(spaces) + "|---#> " + file.getName());
-            }
-
+        GetMatchDetails getMatchDetails = new GetMatchDetails();
+        try {
+            MatchDetailsParser matchDetailsParser = new MatchDetailsParser();
+            JsonObject match = getMatchDetails.getFullMatchDetailsById("7340504781");
+            matchDetailsParser.getPlayersFromJsonObject(match);
+            System.out.println("hero id: " + matchDetailsParser.getPlayersFromJsonObject(match).get(1).getAsJsonObject().get("hero_id"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println(space.repeat(directory_spaces) + "|");
-        if (spaces >= 4 ) spaces = spaces -4;
-//        directory_spaces += 4;
+//        new MeiDOTAnakaFrrame_m();
+
+
     }
 }
