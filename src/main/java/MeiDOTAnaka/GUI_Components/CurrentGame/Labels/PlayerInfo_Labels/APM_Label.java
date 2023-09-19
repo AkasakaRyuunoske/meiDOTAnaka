396f40450d97bcceba2d3ce2e934d41e25b91de1
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-09-19 10:04:09 +0200

    foreground added

--- src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Labels/PlayerInfo_Labels/APM_Label.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/CurrentGame/Labels/PlayerInfo_Labels/APM_Label.java
@@ -1,9 +1,11 @@
 package MeiDOTAnaka.GUI_Components.CurrentGame.Labels.PlayerInfo_Labels;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class APM_Label extends JLabel {
     public APM_Label(){
+        setForeground(Color.magenta);
         setText("Current APM: ");
     }
 }
