f4d2968c2ad30b19a5e5f5d236064d41a669b60e
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-10-02 17:04:51 +0200

    new button added

---
+++ src/main/java/MeiDOTAnaka/GUI_Components/MainFrame/Buttons_Component/CringeList_Button.java
@@ -0,0 +1,56 @@
+package MeiDOTAnaka.GUI_Components.MainFrame.Buttons_Component;
+
+import MeiDOTAnaka.GUI_Components.MainFrame.MainPanel.State_Panel;
+import MeiDOTAnaka.GUI_Components.MainFrame.MeiDOTAnakaFrrame_m;
+import MeiDOTAnaka.GUI_Components.MainFrame.Selected_Component.Selected_Panel;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class CringeList_Button extends JButton implements ActionListener, MeiDOTAnaka_Button {
+    MeiDOTAnakaFrrame_m meiDOTAnakaFrame;
+    Selected_Panel selectedPanel;
+    State_Panel state_panel;
+
+    public CringeList_Button(){
+        setText("Cringe List");
+
+        setBackground(null);
+        setFocusable(false);
+
+        try {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, new File("src\\main\\java\\MeiDOTAnaka\\Resources\\Fonts\\OldEnglishTextMT.ttf"));
+            Font sizedFont = font.deriveFont(Font.BOLD, 28f);
+            setFont(sizedFont);
+            setForeground(new Color(111, 24, 211));
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        addActionListener(this::actionPerformed);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this) {
+            System.out.println("cringe list was called");
+        }
+    }
+
+    @Override
+    public void setContext(@NotNull MeiDOTAnakaFrrame_m context_frame, @NotNull Selected_Panel context_panel, @NotNull State_Panel state_panel) {
+        this.meiDOTAnakaFrame = context_frame;
+        this.selectedPanel    = context_panel;
+        this.state_panel      = state_panel;
+    }
+
+    @Override
+    public void switchPanel() {
+
+    }
+}
