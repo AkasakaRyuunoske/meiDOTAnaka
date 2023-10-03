4521958dedde319cf3e40e68335c36d92b0564c5
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-10-03 17:03:15 +0200

    Configuration now is somewhat supported

--- src/main/java/MeiDOTAnaka/Services/Configuration/ConfigurationManager.java
+++ src/main/java/MeiDOTAnaka/Services/Configuration/ConfigurationManager.java
@@ -33,6 +33,7 @@ public class ConfigurationManager {
     }
 
     public void readConfigurationFile(){
+        fileContents = "";
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(configurationFile))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
@@ -62,17 +63,29 @@ public class ConfigurationManager {
         System.out.println(buttonsArray.get(1));
 
         for (JsonElement jsonElement: buttonsArray){
+
             System.out.println("jButton.getName(): " + jButton.getName());
             System.out.println("jsonElement.toString(): " + jsonElement.getAsJsonObject().get("name").toString());
-            if (jsonElement.getAsJsonObject().get("name").toString().equals("\"" + jButton.getName() + "\"")) {
-                System.out.println("Found!");
-                jButton.setBackground(Color.PINK);
-                jButton.setForeground(Color.WHITE);
-                jButton.setText("Test");
+
+            if (jsonElement.getAsJsonObject().get("name").toString().replace("\"", "").equals(jButton.getName())) {
+
+                int background_color_R = Integer.parseInt(jsonElement.getAsJsonObject().get("background_color").getAsJsonArray().get(0).getAsJsonObject().get("r").toString());
+                int background_color_G = Integer.parseInt(jsonElement.getAsJsonObject().get("background_color").getAsJsonArray().get(1).getAsJsonObject().get("g").toString());
+                int background_color_B = Integer.parseInt(jsonElement.getAsJsonObject().get("background_color").getAsJsonArray().get(2).getAsJsonObject().get("b").toString());
+
+                int foreground_color_R = Integer.parseInt(jsonElement.getAsJsonObject().get("foreground_color").getAsJsonArray().get(0).getAsJsonObject().get("r").toString());
+                int foreground_color_G = Integer.parseInt(jsonElement.getAsJsonObject().get("foreground_color").getAsJsonArray().get(1).getAsJsonObject().get("g").toString());
+                int foreground_color_B = Integer.parseInt(jsonElement.getAsJsonObject().get("foreground_color").getAsJsonArray().get(2).getAsJsonObject().get("b").toString());
+
+                jButton.setBackground(new Color(background_color_R, background_color_G, background_color_B));
+                jButton.setForeground(new Color(foreground_color_R, foreground_color_G, foreground_color_B));
+
+                jButton.setText(jsonElement.getAsJsonObject().get("text").toString().replace("\"", ""));
 
                 jButton.revalidate();
                 jButton.repaint();
             }
+
         }
     }
 }
