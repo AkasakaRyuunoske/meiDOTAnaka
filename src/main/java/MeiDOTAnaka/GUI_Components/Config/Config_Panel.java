04e33ccf92429711a8a315eba0ceb9f27809705e
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-10-03 17:02:59 +0200

    SetContext method and new button added

--- src/main/java/MeiDOTAnaka/GUI_Components/Config/Config_Panel.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/Config/Config_Panel.java
@@ -1,10 +1,19 @@
 package MeiDOTAnaka.GUI_Components.Config;
 
+import MeiDOTAnaka.GUI_Components.MainFrame.MeiDOTAnakaFrrame_m;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Config_Panel extends JPanel {
+    private ImportConfigFile_Button getConfigFileButton;
     public Config_Panel(){
+        getConfigFileButton = new ImportConfigFile_Button();
+        add(getConfigFileButton);
         setBackground(new Color(17,17,17));
+    }
+
+    public void setContextFrame(MeiDOTAnakaFrrame_m contextFrame){
+        getConfigFileButton.meiDOTAnakaFrame = contextFrame;
     }
 }
