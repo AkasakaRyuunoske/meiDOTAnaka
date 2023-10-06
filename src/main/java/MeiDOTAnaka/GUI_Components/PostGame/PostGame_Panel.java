9e2b0725e361bc31ed7966c1747fd71933b5fc9f
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-10-06 13:43:44 +0200

    component renamed(heroKills -> matchShortOverview)

--- src/main/java/MeiDOTAnaka/GUI_Components/PostGame/PostGame_Panel.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/PostGame/PostGame_Panel.java
@@ -1,9 +1,8 @@
 package MeiDOTAnaka.GUI_Components.PostGame;
 
 import MeiDOTAnaka.GUI_Components.PostGame.Panels.HeroImportantStats.HeroImportantStats_Panel;
-import MeiDOTAnaka.GUI_Components.PostGame.Panels.HeroInventory.HeroInventory_Panel;
 import MeiDOTAnaka.GUI_Components.PostGame.Panels.HeroItemsAndBuffs_Panel;
-import MeiDOTAnaka.GUI_Components.PostGame.Panels.HeroKillsStats_Panel;
+import MeiDOTAnaka.GUI_Components.PostGame.Panels.MatchShortOverview.MatchShortOverview_Panel;
 import lombok.Getter;
 
 import javax.swing.*;
@@ -13,7 +12,7 @@ import java.awt.*;
 public class PostGame_Panel extends JPanel {
     public HeroImportantStats_Panel heroImportantStats_panel;
     public HeroItemsAndBuffs_Panel heroItemsAndBuffs_panel;
-    public HeroKillsStats_Panel heroKillsStats_panel;
+    public MatchShortOverview_Panel matchShortOverview_panel;
 
     public PostGame_Panel(){
         setBackground(Color.RED);
@@ -21,10 +20,10 @@ public class PostGame_Panel extends JPanel {
 
         heroImportantStats_panel = new HeroImportantStats_Panel();
         heroItemsAndBuffs_panel = new HeroItemsAndBuffs_Panel();
-        heroKillsStats_panel = new HeroKillsStats_Panel();
+        matchShortOverview_panel = new MatchShortOverview_Panel();
 
         add(heroItemsAndBuffs_panel);
         add(heroImportantStats_panel);
-        add(heroKillsStats_panel);
+        add(matchShortOverview_panel);
     }
 }
