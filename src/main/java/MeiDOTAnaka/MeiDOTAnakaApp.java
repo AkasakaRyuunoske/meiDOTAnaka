32640aa34c4ece2faf1ac22287c9d1141df81861
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-10-10 16:11:52 +0200

    moved to be visible (temporally)

--- src/main/java/MeiDOTAnaka/MeiDOTAnakaApp.java
+++ src/main/java/MeiDOTAnaka/MeiDOTAnakaApp.java
@@ -1,3 +1,5 @@
+package MeiDOTAnaka;
+
 import MeiDOTAnaka.GUI_Components.MainFrame.MeiDOTAnakaFrrame_m;
 import MeiDOTAnaka.Services.GSS.GSS;
 
@@ -5,10 +7,11 @@ import MeiDOTAnaka.Services.GSS.GSS;
  * Remember to enable -gamestateintegration as launch option for dota.
  */
 public class MeiDOTAnakaApp {
+    public static MeiDOTAnakaFrrame_m meiDOTAnakaFrrame_m;
     public static void main(String[] args) {
         System.out.println("Application started");
 
-        MeiDOTAnakaFrrame_m meiDOTAnakaFrrame_m = new MeiDOTAnakaFrrame_m();
+        meiDOTAnakaFrrame_m = new MeiDOTAnakaFrrame_m();
         meiDOTAnakaFrrame_m.setButtonsContext(meiDOTAnakaFrrame_m);
 
 //        GSS gss = new GSS();
