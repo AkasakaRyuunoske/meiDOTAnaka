82999b073b9ea4046747dbeeaf1b0e786b1944b1
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-10-22 16:26:32 +0200

    minor change

--- src/main/java/MeiDOTAnaka/GUI_Components/PostGame/Panels/MatchShortOverview/MatchShortOverview_Panel.java
+++ src/main/java/MeiDOTAnaka/GUI_Components/PostGame/Panels/MatchShortOverview/MatchShortOverview_Panel.java
@@ -6,14 +6,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MatchShortOverview_Panel extends JPanel {
-    ItemsOptions_Panel shortOverviewOptionsPanel;
 
     public MatchShortOverview_Panel(){
         setLayout(new GridLayout(2,1));
         setBackground(Color.lightGray);
 
-        shortOverviewOptionsPanel = new ItemsOptions_Panel();
-
-        add(shortOverviewOptionsPanel);
     }
 }
